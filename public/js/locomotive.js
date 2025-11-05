79f574b659b94cf76fe6e85f22cd3b563e29d966
Abhishek-Mahato2982 <abhishekmahato2982@gmail.com>
2025-11-05 23:39:33 +0530

    Update locomotive.js

--- public/js/locomotive.js
+++ public/js/locomotive.js
@@ -1,4 +1,14 @@
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
-    smooth: true
+    smooth: true,
+    smartphone: {
+        smooth: false
+    },
+    tablet: {
+        smooth: false
+    }
 });
+
+// Prevent Locomotive from affecting body/html
+document.body.style.overflow = 'visible';
+document.documentElement.style.overflow = 'visible';
